0299d31b4307c1245ea01fdf9bd674eefc404595
adityap5 <aditya29pippal@gmail.com>
2023-11-11 01:18:22 +0530

    react roter set

--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import Image from 'next/image'
+import { Link } from 'react-router-dom'
 const Navbar = () => {   
     const [menu, setmenu] = useState("shop") 
   return (
@@ -17,19 +18,18 @@ const Navbar = () => {
     <p>acult</p>
 </div>
 <ul className="menu">
-    <li onClick={()=>{setmenu("shop")}}>Shop{menu === "shop"? <hr />:<></>} </li>
-    <li onClick={()=>{setmenu("mens")}}>Mens {menu === "mens"? <hr />:<></>}</li>
-    <li onClick={()=>{setmenu("womens")}}>Womens {menu === "womens"? <hr />:<></>}</li>
-    <li onClick={()=>{setmenu("kids")}}>Kids {menu === "kids"? <hr />:<></>}</li>
+    <li onClick={()=>{setmenu("shop")}}><Link to="/" style={{textDecoration:'none' ,color:'grey' }}>Shop</Link>{menu === "shop"? <hr />:<></>} </li>
+    <li onClick={()=>{setmenu("mens")}}><Link to="/mens" style={{textDecoration:'none' ,color:'grey' }}>Mens</Link>  {menu === "mens"? <hr />:<></>}</li>
+    <li onClick={()=>{setmenu("womens")}}><Link to="/womens" style={{textDecoration:'none' ,color:'grey' }}>Womens</Link>  {menu === "womens"? <hr />:<></>}</li>
+    <li onClick={()=>{setmenu("kids")}}><Link to="/kids" style={{textDecoration:'none' ,color:'grey' }}>Kids</Link>   {menu === "kids"? <hr />:<></>}</li>
 
 </ul>
 <div className="navbar-btn"> 
-<button>Log In</button>
-<Image
+<Link to ='/login'><button>Log In</button></Link>
+<Link to ='/cart'><Image
       src={cart_icon}
-    
       alt="cart"
-    />
+    /></Link>
     <div className="carticon">0</div>
 </div>
 </div>
