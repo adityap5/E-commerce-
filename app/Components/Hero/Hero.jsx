7300a28b277d3e9b8a3dc07ae5653ddfa6f29fb2
adityap5 <aditya29pippal@gmail.com>
2023-11-11 18:41:46 +0530

    hero section

--- app/Components/Hero/Hero.jsx
+++ app/Components/Hero/Hero.jsx
@@ -8,36 +8,35 @@ import hero_image from '../Assets/hero_image.png'
 const Hero = () => {
   return (
     <>
-    <div className="hero">
-    <div className="hero-left">
-<h2>new arrivals only</h2>
-    
-    <div>
-    <div className="hand-icon">
-<p>new</p>
-<Image
-      src={hand_icon}
-      alt="hand"
-    />
-    </div>
-    <p>collections</p>
-    <p>for everyone</p>
-    </div>
-    <div className="latest-btn">
-        <div>latest collection</div>
-        <Image
-      src={arrow}
-      alt="arrow"
-    />
-    </div>
-    </div>
-    <div className="hero-right">
-    <Image
-      src={hero_image}
-      alt="arrow"
-    />
-    </div>
-    </div>
+      <div className="hero">
+        <div className="hero-left">
+          <h2>new arrivals only</h2>
+          
+            <div className="hand-icon" >
+              <p>new</p>
+              <Image
+                src={hand_icon}
+                alt="hand"
+              />
+            </div>
+            <p>collections</p>
+            <p>for everyone</p>
+          
+          <div className="latest-btn">
+            <div>latest collection</div>
+            <Image
+              src={arrow}
+              alt="arrow"
+            />
+          </div>
+        </div>
+        <div className="hero-right">
+          <Image
+            src={hero_image}
+            alt="arrow"
+          />
+        </div>
+      </div>
     </>
   )
 }
